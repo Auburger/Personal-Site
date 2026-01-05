3ec7dcbd0a69706f0b90ac70c2a93c07521d296a
Aubrie Chan <164364120+Auburger@users.noreply.github.com>
2026-01-05 11:11:25 -0500

    Add files via upload

---
+++ Website/app.js
@@ -0,0 +1,7 @@
+function closeNav() { // closes the sidebar
+    document.querySelector(".sidebar").style.width = "0";
+}
+
+function openNav() {
+    document.querySelector(".sidebar").style.width = "10vw";
+}
